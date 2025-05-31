8c67212a7f7e0ab90382276f51ef1b1e0410cdd9
Alexander Maslennikov <mirairay20@gmail.com>
2025-05-31 13:37:26 +0300

    Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -8,37 +8,16 @@ url_for_registration = "https://nikita-filonov.github.io/qa-automation-engineer-
 url_for_courses = "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses"
 url_for_create_courses = " https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create"
 
-email = "email@mail.ru"
-username = "ColdApril"
-password = "password"
-
-courses = "Courses"
-there_is_not_result_text = "There is no results"
-results_text = "Results from the load test pipeline will be displayed here"
-
-
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page, ):
-    chromium_page_with_state.goto(url_for_courses)
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit(url_for_courses)
 
-    # Проверка наличия заголовка
-    label_courses = chromium_page_with_state.get_by_test_id("courses-list-toolbar-title-text")
-    expect(label_courses).to_be_visible()
-    expect(label_courses).to_have_text(courses)
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible("username")
 
-    # Проверка наличия иконки
-    icon_folder = chromium_page_with_state.get_by_test_id("courses-list-empty-view-icon")
-    expect(icon_folder).to_be_visible()
-
-    # Проверка наличия надписи There is no results
-    there_is_no_result = chromium_page_with_state.get_by_test_id("courses-list-empty-view-title-text")
-    expect(there_is_no_result).to_have_text(there_is_not_result_text)
-
-    # Проверка наличия надписи Results from the load test pipeline will be displayed here
-    result_from_the_load = chromium_page_with_state.get_by_test_id("courses-list-empty-view-description-text")
-    expect(result_from_the_load).to_have_text(results_text)
-
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_empty_view()
 
 @pytest.mark.courses
 @pytest.mark.regression
