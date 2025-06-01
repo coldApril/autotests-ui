921ccc722ca20571c6e314e1e2fad8be463180fc
Alexander Maslennikov <mirairay20@gmail.com>
2025-06-01 13:55:35 +0300

    Знакомство с PageFactory. Практика работы с Page Factory

--- components/dashboard/dashboard_toolbar_view_component.py
+++ components/dashboard/dashboard_toolbar_view_component.py
@@ -1,12 +1,15 @@
+from playwright.sync_api import Page, expect
+
 from components.base_component import BaseComponent
-from playwright.sync_api import expect
+from elements.text import Text
+
 
 class DashboardToolbarViewComponent(BaseComponent):
-    def __init__(self, page):
+    def __init__(self, page: Page):
         super().__init__(page)
 
-        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        self.title = Text(page, 'dashboard-toolbar-title-text', "Dashboard")
 
-    def check_visible_dashboard_title(self):
-        expect(self.dashboard_title).to_be_visible()
-        expect(self.dashboard_title).to_have_text('Dashboard')
+    def check_visible(self):
+        self.title.check_visible()
+        self.title.check_have_text('Dashboard')
--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,4 +1,6 @@
 from components.authentication.registration_form_component import RegistrationFormComponent
+from elements.button import Button
+from elements.text import Text
 from pages.base_page import BasePage
 from playwright.sync_api import expect
 
@@ -8,8 +10,8 @@ class RegistrationPage(BasePage):
 
         self.registration_form = RegistrationFormComponent(page)
         
-        self.registration_button = page.get_by_test_id('registration-page-registration-button')
-        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        self.registration_button = Button(page, 'registration-page-registration-button', "Registration")
+        self.dashboard_title = Text(page, 'dashboard-toolbar-title-text', "Dashboard")
 
     def click_registration_button(self):
         self.registration_button.click()
