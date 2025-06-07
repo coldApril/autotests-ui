14c04e93698c3353ea42dc6a54150610a989d2a6
Alexander Maslennikov <mirairay20@gmail.com>
2025-06-07 14:07:53 +0300

    Добавляем информацию о переменных окружения в Allure отчет.  Практика работы с переменным окружения в Allure

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import platform
+import sys
+
 from enum import Enum
 from typing import Self
 
@@ -35,22 +38,32 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
+    allure_results_dir: DirectoryPath
     browser_state_file: FilePath
+    os_info: str
+    python_version: str
 
     @classmethod
     def initialize(cls) -> Self:
         videos_dir = DirectoryPath("./videos")
         tracing_dir = DirectoryPath("./tracing")
+        allure_results_dir = DirectoryPath("./allure-results")
         browser_state_file = FilePath("browser-state.json")
+        os_info = f'{platform.system()}, {platform.release()}'
+        python_version = sys.version
 
         videos_dir.mkdir(exist_ok=True)
         tracing_dir.mkdir(exist_ok=True)
+        allure_results_dir.mkdir(exist_ok=True)
         browser_state_file.touch(exist_ok=True)
 
         return Settings(
             videos_dir=videos_dir,
             tracing_dir=tracing_dir,
-            browser_state_file=browser_state_file
+            allure_results_dir=allure_results_dir,
+            browser_state_file=browser_state_file,
+            os_info=os_info,
+            python_version=python_version
         )
     
     def get_base_url(self) -> str:
